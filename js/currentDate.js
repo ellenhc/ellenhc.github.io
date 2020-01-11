a054fa891d0f75bea1fec1ded2997121c639b24d
Ellen Carlson <how19034@byui.edu>
2020-01-11 14:35:42 -0800

    add responsive year for copyright

--- js/currentDate.js
+++ js/currentDate.js
@@ -1,2 +1,6 @@
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+document.getElementById("currentYearId").innerHTML = currentYear
+
 let dateLastModified = document.lastModified;
 document.getElementById("currentDate").innerHTML = dateLastModified;
