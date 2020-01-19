743bf7fe51d1a271026a4fb8cb57a47473797d73
Ellen Carlson <how19034@byui.edu>
2020-01-18 22:14:54 -0800

    added comments

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,6 +1,8 @@
-let currentDate = new Date(); // get current date
+//gets current year for copyright
+let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
 document.getElementById("currentYearId").innerHTML = currentYear
 
+//last modified date
 let dateLastModified = document.lastModified;
 document.getElementById("dateModified").innerHTML = dateLastModified;
