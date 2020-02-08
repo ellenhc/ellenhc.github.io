17ffc55769b1c680c060cd2b0ef866e0fba42e71
ellenhc <how19034@byui.edu>
2020-02-08 00:45:57 -0800

    moved banner css to js so the yellow bar isn't visible on non banner days

--- lesson5/js/currentDate.js
+++ lesson5/js/currentDate.js
@@ -33,4 +33,7 @@ for (i = currentDayNumber; i < endDay; i++) {
 if (currentDayNumber == 5){
   let text = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
   document.getElementById("banner").innerHTML = text;
+  document.getElementById("banner").style.backgroundColor = "#FFD82D";
+  document.getElementById("banner").style.textAlign = "center";
+  document.getElementById("banner").style.padding = "0.75em";
 }
