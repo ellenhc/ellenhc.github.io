a07ed4e99f574b36a12a5a2d7ce52305a95b339b
Ellen Carlson <how19034@byui.edu>
2020-02-15 11:32:50 -0800

    put banner js into its own folder, upped contrast for 5 day forecast table, added srcset for header img

--- lesson6/js/currentDate.js
+++ lesson6/js/currentDate.js
@@ -22,18 +22,9 @@ document.getElementById("currentTimeId").innerHTML = currentWeekday + ", " + cur
 let currentDayNumber = currentTime.getDay();
 let endDay = currentDayNumber + 5;
 for (i = currentDayNumber; i < endDay; i++) {
-  let nextDayNumber = i % 7;
-  let nextDay = days[nextDayNumber];
-  let j = i - currentDayNumber;
-  let x = document.getElementsByClassName("dayClass");
-  x[j].innerHTML = nextDay;
-}
-
-//Displays banner if it's Friday
-if (currentDayNumber == 5){
-  let text = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-  document.getElementById("banner").innerHTML = text;
-  document.getElementById("banner").style.backgroundColor = "#FFD82D";
-  document.getElementById("banner").style.textAlign = "center";
-  document.getElementById("banner").style.padding = "0.75em";
+    let nextDayNumber = i % 7;
+    let nextDay = days[nextDayNumber];
+    let j = i - currentDayNumber;
+    let x = document.getElementsByClassName("dayClass");
+    x[j].innerHTML = nextDay;
 }
