087c08722d59fb371124de5d4877dcd00fd0179c
Ellen Carlson <how19034@byui.edu>
2020-02-15 22:03:27 -0800

    changed picnic banner code so there isn't an empty div

--- lesson6/js/picnicBanner.js
+++ lesson6/js/picnicBanner.js
@@ -1,8 +1,8 @@
 //Displays banner if it's Friday
 if (currentDayNumber == 5) {
-    let text = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-    document.getElementById("banner").innerHTML = text;
     document.getElementById("banner").style.backgroundColor = "#FFD82D";
     document.getElementById("banner").style.textAlign = "center";
     document.getElementById("banner").style.padding = "0.75em";
+} else {
+    document.getElementById("banner").style.display = "none";
 }
