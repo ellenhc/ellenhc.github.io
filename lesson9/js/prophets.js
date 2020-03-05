2c5077aab46d0d5b166032772119fa982248ce35
Ellen Carlson <how19034@byui.edu>
2020-03-05 11:23:26 -0800

    finished javascript code for prophets.html

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,21 +1,33 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
-    .then(function (response) {
+    .then(function(response) {
+        //console.log(response);
         return response.json();
     })
-    .then(function (jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
-        for (let i = 0; i < prophets.length; i++) {}
+    .then(function(jsonObject) {
+        //console.table(jsonObject);
+        const prophets = jsonObject['prophets'];
+        for (let i = 0; i < prophets.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let dateOfBirth = document.createElement('p');
+            let placeOfBirth = document.createElement('p');
+            let img = document.createElement('img');
+
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+            dateOfBirth.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+
+            placeOfBirth.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+
+            img.src = prophets[i].imageurl;
+
+            card.appendChild(h2);
+            card.appendChild(dateOfBirth);
+            card.appendChild(placeOfBirth);
+            card.appendChild(img);
+
+            document.querySelector('div.cards').appendChild(card);
+        }
     });
-
-const prophets = jsonObject['prophets'];
-
-let card = document.createElement('section');
-let h2 = document.createElement('h2');
-
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-card.appendChild(h2);
-
-document.querySelector('div.cards').appendChild(card);
