de18eaf581742cf92111ee4c4b7473e025202392
Ellen Carlson <how19034@byui.edu>
2020-03-07 15:29:20 -0800

    added the JSON stuff

--- lesson9/js/homePage.js
+++ lesson9/js/homePage.js
@@ -7,26 +7,34 @@ fetch(requestURL)
     })
     .then(function(jsonObject) {
         //console.table(jsonObject);
-        const prophets = jsonObject['prophets'];
-        for (let i = 0; i < prophets.length; i++) {
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
-            let dateOfBirth = document.createElement('p');
-            let placeOfBirth = document.createElement('p');
+            let motto = document.createElement('p');
+            let yearFounded = document.createElement('p');
+            let population = document.createElement('p');
+            let rainfall = document.createElement('p');
             let img = document.createElement('img');
 
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            h2.textContent = towns[i].name;
 
-            dateOfBirth.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            motto.textContent = towns[i].motto;
 
-            placeOfBirth.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+            yearFounded.textContent = "Year founded: " + towns[i].yearFounded;
 
-            img.setAttribute('src', prophets[i].imageurl);
-            img.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+            population.textContent = "Population: " + towns[i].currentPopulation;
+
+            rainfall.textContent = "Average rainfall: " + towns[i].averageRainfall;
+
+            img.setAttribute('src', towns[i].photo);
+            img.setAttribute('alt', "Photo of " + towns[i].name);
 
             card.appendChild(h2);
-            card.appendChild(dateOfBirth);
-            card.appendChild(placeOfBirth);
+            card.appendChild(motto);
+            card.appendChild(yearFounded);
+            card.appendChild(population);
+            card.appendChild(rainfall);
             card.appendChild(img);
 
             document.querySelector('div.cards').appendChild(card);
