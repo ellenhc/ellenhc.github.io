f20f27ffd80b6abdd8e5e056394e91af0e14a559
Ellen Carlson <how19034@byui.edu>
2020-03-09 18:58:47 -0700

    added lesson 10 folder and api test files. added link to assignment portal

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,11 @@
+const apiURL = "api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=de0b9a3291e852137733bf4abab06ac0";
+//APPID=de0b9a3291e852137733bf4abab06ac0
+//api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
+//Preston city Id = 5604473
+//&units=imperial
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+    });
