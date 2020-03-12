a7ac53a6a272d42c6e6b386667d6a21f7e7ddb73
Ellen Carlson <how19034@byui.edu>
2020-03-12 10:19:27 -0700

    changed hero images in lesson 6 back to ones without text indicating size, added lesson10 html, etc

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,8 +1,9 @@
-const apiURL = "api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=de0b9a3291e852137733bf4abab06ac0";
-//APPID=de0b9a3291e852137733bf4abab06ac0
-//api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
-//Preston city Id = 5604473
-//&units=imperial
+const apiURL = "api.openweathermap.org/data/2.5/weather?id=5604473&appid=de0b9a3291e852137733bf4abab06ac0&units=imperial"
+    //&units=imperial&appid=de0b9a3291e852137733bf4abab06ac0";
+    //APPID=de0b9a3291e852137733bf4abab06ac0
+    //api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
+    //Preston city Id = 5604473
+    //&units=imperial
 
 fetch(apiURL)
     .then((response) => response.json())
