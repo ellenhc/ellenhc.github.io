a6f31feb09dbe60d15f5f3e4ae2ab4802d4769be
Ellen Carlson <how19034@byui.edu>
2020-03-31 23:44:17 -0700

    almost finished footer and testimonials

--- termProject/js/currentDate.js
+++ termProject/js/currentDate.js
@@ -2,18 +2,18 @@
 
 let currentTime = new Date();
 
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; //Use an array to display the name of the weekday
+/*var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; //Use an array to display the name of the weekday
 let currentWeekday = days[currentTime.getDay()]; //Returns the weekday of a date
 
 let currentDate = currentTime.getDate(); //Returns the day of a date as a number
 
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; //Use an array to display the name of the month
-let currentMonth = months[currentTime.getMonth()]; //Returns the month of a date as a number
+let currentMonth = months[currentTime.getMonth()]; //Returns the month of a date as a number*/
 
 let currentYear = currentTime.getFullYear(); //Returns the year of a date as a four digit number
 
 //Shows current year next to copyright symbol in footer
 document.getElementById("currentYearId").innerHTML = currentYear;
 
-//Shows today's date in footer
-document.getElementById("currentTimeId").innerHTML = currentWeekday + ", " + currentDate + " " + currentMonth + " " + currentYear;
+/*//Shows today's date in footer
+document.getElementById("currentTimeId").innerHTML = currentWeekday + ", " + currentDate + " " + currentMonth + " " + currentYear;*/
---
+++ termProject/js/weather.js
@@ -0,0 +1,29 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5606338&appid=de0b9a3291e852137733bf4abab06ac0&units=imperial";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        //document.getElementById('currentlyId').textContent = jsObject.weather[0].description;
+        document.getElementById('tempId').textContent = Math.round(jsObject.main.temp);
+        document.getElementById('humidityId').textContent = jsObject.main.humidity;
+        document.getElementById('windSpeedId').textContent = Math.round(jsObject.wind.speed);
+        //document.getElementById('windChillId').textContent = getWindChill();
+        const iconName = jsObject.weather[0].icon;
+        const iconsrc = `https://openweathermap.org/img/w/${iconName}.png`;
+        const altText = jsObject.weather.description;
+        document.getElementById('icon').setAttribute('src', iconsrc);
+        document.getElementById('icon').setAttribute('alt', altText);
+    });
+
+/*function getWindChill() {
+    let tString = document.getElementById("tempId").innerHTML;
+    let t = Number(tString);
+    let sString = document.getElementById("windSpeedId").innerHTML;
+    let s = Number(sString);
+    if (t <= 50 && s > 3) {
+        let windChill = 35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275 * t * (s ** 0.16));
+        return Math.round(windChill);
+    } else {
+        return "N/A";
+    }
+}*/
