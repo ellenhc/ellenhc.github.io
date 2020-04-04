b16ad975784c3a713a39f948e606bf569c39fb65
Ellen Carlson <how19034@byui.edu>
2020-04-03 23:38:32 -0700

    added 5 day forecast and styled weather divs

--- termProject/js/weather.js
+++ termProject/js/weather.js
@@ -1,4 +1,5 @@
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5606338&appid=de0b9a3291e852137733bf4abab06ac0&units=imperial";
+const apiForecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5606338&appid=de0b9a3291e852137733bf4abab06ac0&units=imperial";
 
 fetch(apiURL)
     .then((response) => response.json())
@@ -7,7 +8,7 @@ fetch(apiURL)
         document.getElementById('tempId').textContent = Math.round(jsObject.main.temp);
         document.getElementById('humidityId').textContent = jsObject.main.humidity;
         document.getElementById('windSpeedId').textContent = Math.round(jsObject.wind.speed);
-        //document.getElementById('windChillId').textContent = getWindChill();
+        document.getElementById('windChillId').textContent = getWindChill();
         const iconName = jsObject.weather[0].icon;
         const iconsrc = `https://openweathermap.org/img/w/${iconName}.png`;
         const altText = jsObject.weather[0].description;
@@ -15,15 +16,42 @@ fetch(apiURL)
         document.getElementById('icon').setAttribute('alt', altText);
     });
 
-/*function getWindChill() {
+function getWindChill() {
     let tString = document.getElementById("tempId").innerHTML;
     let t = Number(tString);
     let sString = document.getElementById("windSpeedId").innerHTML;
     let s = Number(sString);
     if (t <= 50 && s > 3) {
         let windChill = 35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275 * t * (s ** 0.16));
-        return Math.round(windChill);
+        return Math.round(windChill) + " mph";
     } else {
         return "N/A";
     }
-}*/
+}
+
+//Supplies the dates for five day forecast
+let currentDay = new Date();
+let currentDayNumber = currentDay.getDay() + 1;
+let endDay = currentDayNumber + 5;
+for (i = currentDayNumber; i < endDay; i++) {
+    let nextDayNumber = i % 7;
+    let nextDay = days[nextDayNumber];
+    let nextDayShort = nextDay.substring(0, 3);
+    let j = i - currentDayNumber;
+    let x = document.getElementsByClassName("dayClass");
+    x[j].innerHTML = nextDayShort;
+}
+
+fetch(apiForecastURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        const fiveDayForecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+        for (let i = 0; i < fiveDayForecast.length; i++) {
+            document.getElementById(`temp${i+1}`).textContent = Math.round(fiveDayForecast[i].main.temp);
+            const iconName = fiveDayForecast[i].weather[0].icon;
+            const iconsrc = `https://openweathermap.org/img/w/${iconName}.png`;
+            const altText = fiveDayForecast[i].weather[0].description;
+            document.getElementById(`icon-${i+1}`).setAttribute('src', iconsrc);
+            document.getElementById(`icon-${i+1}`).setAttribute('alt', altText)
+        }
+    });
