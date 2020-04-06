885856a2d9b817bd659608224c1f1ed3dc49bf04
Ellen Carlson <how19034@byui.edu>
2020-04-06 14:59:42 -0700

    fixed json file error and now river guides page should pull in info from json file

--- termProject/js/riverGuides.js
+++ termProject/js/riverGuides.js
@@ -3,15 +3,15 @@ const jsonURL = "https://ellenhc.github.io/termProject/riverguides.json";
 fetch(jsonURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        for (let i = 0; i < jsObject.length; i++) {
+        for (let i = 0; i < jsObject.guides.length; i++) {
             document.getElementById(`name${i+1}`).textContent = jsObject.guides[i].name;
             const picName = jsObject.guides[i].photo;
             const picSource = `https://ellenhc.github.io/termProject/images/${picName}.jpg`;
-            const altText = "portrait of " + guides[i].name;
+            const altText = "portrait of " + jsObject.guides[i].name;
             document.getElementById(`pic${i+1}`).setAttribute('src', picSource);
             document.getElementById(`pic${i+1}`).setAttribute('alt', altText);
-            document.getElementById(`email${i+1}`).textContent = guides[i].email;
-            document.getElementById(`cert${i+1}`).textContent = guides[i].certification;
-            document.getElementById(`bio${i+1}`).textContent = guides[i].biography;
+            document.getElementById(`email${i+1}`).textContent = jsObject.guides[i].email;
+            document.getElementById(`cert${i+1}`).textContent = jsObject.guides[i].certification;
+            document.getElementById(`bio${i+1}`).textContent = jsObject.guides[i].biography;
         }
     });
