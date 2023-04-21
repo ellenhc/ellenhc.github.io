4ab50d16616bfcfc26e5a044639eaca262e4ffc5
Ellen Carlson <ellenhc@users.noreply.github.com>
2023-04-21 14:14:18 -0500

    deleted old files

--- js/currentdate.js
+++
@@ -1,8 +0,0 @@
-//gets current year for copyright
-let currentDate = new Date();
-let currentYear = currentDate.getFullYear();
-document.getElementById("currentYearId").innerHTML = currentYear
-
-//last modified date
-let dateLastModified = document.lastModified;
-document.getElementById("dateModified").innerHTML = dateLastModified;
